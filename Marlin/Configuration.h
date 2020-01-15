9dab76c338cb443bfda5e2c7f05d480c8df6df85
Anders Sahlman <anders.sahlman@gmail.com>
2020-01-15 19:04:26 +0100

    Increasing max extrusion length for filament unload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -567,7 +567,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 800
+#define EXTRUDE_MAXLENGTH 1000
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
